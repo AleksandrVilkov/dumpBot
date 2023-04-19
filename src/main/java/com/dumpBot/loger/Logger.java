5491b2206fab977a0e838f57e7851dbb80097b60
vilkov <alexander.vilkov@gridnine.com>
2023-04-19 12:52:01 +0300

    logger

--- src/main/java/com/dumpBot/loger/Logger.java
+++ src/main/java/com/dumpBot/loger/Logger.java
@@ -2,8 +2,20 @@ package com.dumpBot.loger;
 
 import org.springframework.stereotype.Component;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
 @Component
 public class Logger implements ILogger {
+
+    private final Path PATH;
+
+    public Logger() {
+        PATH = Path.of("");
+
+    }
+
     @Override
     public void writeInfo(String msg) {
         System.out.println(msg);
@@ -17,5 +29,22 @@ public class Logger implements ILogger {
     @Override
     public void writeError(String msg) {
         System.out.println(msg);
+    }
+
+    public void write(String test) {
+
+        try(FileWriter writer = new FileWriter("notes3.txt", false))
+        {
+            // запись всей строки
+            String text = "Hello Gold!";
+            writer.write(text);
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
