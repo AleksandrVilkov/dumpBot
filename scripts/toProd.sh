2679d3aa5de481d14e4b3ff6622d5e64b6b95996
vilkov <alexander.vilkov@gridnine.com>
2023-04-28 14:45:17 +0300

    database

--- scripts/toProd.sh
+++ scripts/toProd.sh
@@ -9,7 +9,7 @@ echo 'Restart server...'
 
 ssh -i ~/.ssh/id_rsa.pub root@85.193.82.129 << EOF
 pgrep java | xargs kill -9
-nohup java -jar dumpBot-0.0.1-SNAPSHOT.jar > log.txt &
+nohup java -jar -Dspring.profiles.active=prod dumpBot-0.0.1-SNAPSHOT.jar > log.txt &
 EOF
 
 echo 'Bye'
