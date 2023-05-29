8dc5b41ced0af116498fc902091bdda0547ca2c1
vilkov <alexander.vilkov@gridnine.com>
2023-05-29 11:47:21 +0300

    REJECTED

--- src/main/java/com/dumpBot/processor/callbackProcessor/process/RejectedProcess.java
+++ src/main/java/com/dumpBot/processor/callbackProcessor/process/RejectedProcess.java
@@ -1,17 +1,32 @@
 package com.dumpBot.processor.callbackProcessor.process;
 
+import com.dumpBot.model.User;
 import com.dumpBot.model.UserAccommodation;
 import com.dumpBot.processor.callbackProcessor.CallbackProcess;
+import com.dumpBot.storage.storage.UserStorage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class RejectedProcess implements CallbackProcess {
+    @Autowired
+    UserStorage userStorage;
+
     @Override
     public List<SendMessage> start(UserAccommodation userAccommodation, Update update) {
-        return null;
+        //TODO перенести в ресурсы
+        String text = "Укажи причину отклонения запроса. Обрати внимание, это сообщение получит автор, и все остальные администраторы.";
+        SendMessage sendMessage = new SendMessage(String.valueOf(update.getCallbackQuery().getMessage().getFrom().getId()), text);
+        sendMessage.setReplyToMessageId(update.getCallbackQuery().getMessage().getMessageId());
+        User user = userStorage.getUser(String.valueOf(update.getCallbackQuery().getMessage().getFrom().getId()));
+        user.setClientAction("REJECTED");
+        user.setLastCallback(update.getCallbackQuery().getData());
+        userStorage.saveUser(user);
+        return Collections.singletonList(sendMessage);
     }
 }
